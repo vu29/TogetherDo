c4eb041f810ee1f253000fa9de52c1812089dc1a
pulkitvyas08 <e19cse284@bennett.edu.in>
2021-06-26 15:04:54 +0530

    Basic implementation of task list

---
+++ togetherdo-frontend/src/components/AddNewTask.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const AddNewTask = () => <p>Add New Task</p>
--- togetherdo-frontend/src/components/layout/Main-Area.js
+++ togetherdo-frontend/src/components/layout/Main-Area.js
@@ -1,11 +1,11 @@
 import React from 'react';
+
 import { TaskArea } from '../Task-Area';
 import { SidePanel } from './Side-Panel';
 
 export const MainArea = () => {
     return(
-        <div>
-            <p>The main area</p>
+        <div className="main-area">
             <TaskArea />
             <SidePanel />
         </div>
